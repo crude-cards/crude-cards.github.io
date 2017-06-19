e412d26e7b9b5a1ae662fb27e6e0324086542a84
Amish Shah <amishshah.2k@gmail.com>
2017-06-19 13:33:44 +0100

    Target branch needs to be master

--- deploy/.travis-deploy.sh
+++ deploy/.travis-deploy.sh
@@ -14,7 +14,7 @@ echo -e "Building for a stable branch push - building and deploying."
 REPO=`git config remote.origin.url`
 SHA=`git rev-parse --verify HEAD`
 
-TARGET_BRANCH="build"
+TARGET_BRANCH="master"
 git clone $REPO dist -b $TARGET_BRANCH
 
 npm run build
