2e7e544ba146ff14a40926435b8c662f459a63a8
Amish Shah <amishshah.2k@gmail.com>
2017-06-19 15:05:51 +0100

    Add static assets and restructure

---
+++ src/views/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Coming soon k bye</h1>
+</template>
+
+<script>
+export default {
+  name: 'index-view',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
